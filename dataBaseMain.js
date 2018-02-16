82c89666972a473ab4a2f018d5ffe93e50cd22d8
Papp Gergely Flórián <papp.gergely.florian@gmail.com>
2018-02-16 11:43:53 +0100

    killcat added

--- dataBaseMain.js
+++ dataBaseMain.js
@@ -24,6 +24,10 @@ function createTable (table) {
   database[table] = {};
 }
 
+function killCat (key, table) {
+  database[table][key]--;
+}
+
 createTable('cat1');
 createTable('cat2');
 console.log(database);
@@ -31,12 +35,17 @@ putData('name', 'shitbag', 'cat2');
 putData('size', 'even bigger', 'cat2');
 putData('tail', 'small', 'cat2');
 putData('color', 'black', 'cat2');
+putData('remainingLives', 6, 'cat2');
 putData('name', 'asshole', 'cat1');
 putData('size', 'fucking big', 'cat1');
 putData('tail', 'fucking big', 'cat1');
 putData('color', 'tabby', 'cat1');
+putData('remainingLives', 3, 'cat1');
 console.log(database);
 console.log(getData('size', 'cat1'));
 delByKey('size', 'cat2');
 delByValue('fucking big', 'cat1');
+console.log(database);
+killCat('remainingLives', 'cat1');
+killCat('remainingLives', 'cat2');
 console.log(database);
